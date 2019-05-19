79f7f5c7849d2a8b27a636523d8b1063a5979ddb
Richard Schembri <drahcir.sanguin@gmail.com>
2019-05-20 00:14:31 +0900

    Slight tweaks

---
+++ UnityRSToolkit/Assets/RSToolKit/Scripts/UI/Controls/UIPopup.cs
@@ -0,0 +1,88 @@
+﻿namespace RSToolkit.UI.Controls
+{
+    using UnityEngine;
+    using UnityEngine.EventSystems;
+    using RSToolkit.Tools;
+
+    public class UIPopup : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerClickHandler {
+
+    public bool Draggable = false;
+        // Use this for initialization
+
+
+        public void SetPosition(Vector3 position)
+        {
+            var pt = this.transform.position;
+            float xPos = position.x;
+            float yPos = position.y;
+
+            if (xPos < Screen.width * 0.15f)
+            {
+                xPos = Screen.width * 0.15f;
+            }
+            if (xPos > Screen.width * 0.85f)
+            {
+                xPos = Screen.width * 0.85f;
+            }
+
+            if (yPos < Screen.height * 0.325f)
+            {
+                yPos = Screen.height * 0.325f;
+            }
+            if (yPos > Screen.height * 0.70f)
+            {
+                yPos = Screen.height * 0.70f;
+            }
+            this.transform.position = new Vector3(xPos, yPos, pt.z);
+        }
+
+        public void OpenPopup()
+        {
+            this.gameObject.SetActive(true);
+        }
+
+        public void ClosePopup()
+        {
+            this.gameObject.SetActive(false);
+        }
+
+        public void DestroyPopup()
+        {
+            Destroy(this.gameObject);
+        }
+
+        void IEndDragHandler.OnEndDrag(PointerEventData eventData)
+        {
+            if (!Draggable)
+            {
+                return;
+            }
+
+        }
+
+        void IDragHandler.OnDrag(PointerEventData eventData)
+        {
+            if (!Draggable)
+            {
+                return;
+            }
+
+            //transform.position += (Vector3)eventData.delta; //= Input.mousePosition;
+            SetPosition(transform.position + (Vector3)eventData.delta);
+        }
+
+        void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
+        {
+            this.transform.SetAsLastSibling();
+            if (!Draggable)
+            {
+                return;
+            }
+        }
+
+        void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+        {
+            this.transform.SetAsLastSibling();
+        }
+    }
+}
