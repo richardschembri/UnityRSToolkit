fc962131cc63823416f68021a6da66870a8fc081
Richard <richard.schembri@tongullman.co.jp>
2019-05-20 18:47:04 +0900

    Countdown functionality

---
+++ UnityRSToolkit/Assets/RSToolKit/Scripts/General/Controls/CountDown.cs
@@ -0,0 +1,71 @@
+﻿namespace RSToolkit.Controls
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.Events;
+
+    public class CountDown : MonoBehaviour
+    {
+        public int CountFrom = 10;
+
+        private bool m_isRunning = false;
+        public bool IsRunning{
+            get{
+                return m_isRunning;
+            }
+            private set{
+                m_isRunning = value;
+            }
+        }
+        private int m_Count = 0;
+        public int Count{
+            get{ return m_Count; }
+            private set { m_Count = value; }
+        }
+        public float SecondsInterval = 1;
+
+        public UnityEvent OnStart = new UnityEvent();
+        public UnityEvent OnTick = new UnityEvent();
+        public UnityEvent OnReset = new UnityEvent();
+        public UnityEvent OnComplete = new UnityEvent();
+
+        public void ResetCounter(){
+            Count = CountFrom;
+            OnReset.Invoke();
+        }
+        public void StartCountdown(bool resume = false){
+            if(IsRunning){
+                return;
+            }
+
+            if (!resume){
+                ResetCounter();
+            }
+            StopCountdown();
+            IsRunning = true;
+            OnStart.Invoke();
+            InvokeRepeating("DecrementCounter", SecondsInterval, SecondsInterval);
+        }
+        public void StopCountdown(){
+            CancelInvoke("DecrementCounter");
+            IsRunning = false;
+        }
+
+        public bool IsComplete(){
+            return Count <= 0;
+        }
+
+        void DecrementCounter(){
+            Count--;
+            OnTick.Invoke();
+            if (IsComplete()){
+                CancelInvoke("DecrementCounter");
+                OnComplete.Invoke();
+            }        
+        }
+        
+    }
+
+}
---
+++ UnityRSToolkit/Assets/RSToolKit/Scripts/UI/Controls/UICountDownText.cs
@@ -0,0 +1,33 @@
+﻿namespace RSToolkit.UI.Controls
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;
+    using RSToolkit.Controls;
+    using UnityEngine.Events;
+
+    [RequireComponent(typeof(Text))]
+    public class UICountDownText : CountDown
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            OnStart.AddListener(onTick);
+            OnTick.AddListener(onTick);
+            OnReset.AddListener(onTick);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+        void onStart(){
+
+        }
+        void onTick(){
+            this.GetComponent<Text>().text = Count.ToString();
+        }
+    }
+}
