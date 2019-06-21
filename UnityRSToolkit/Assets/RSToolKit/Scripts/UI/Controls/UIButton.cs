b0f7fc6509bb5bece991b5a2b45f2d1e351284e7
Richard <richard.schembri@tongullman.co.jp>
2019-06-21 10:13:06 +0900

    Add Scroll bar with scroll buttons

---
+++ UnityRSToolkit/Assets/RSToolKit/Scripts/UI/Controls/UIButton.cs
@@ -0,0 +1,39 @@
+﻿namespace RSToolkit.UI.Controls
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.Events;
+    using UnityEngine.EventSystems;
+    using UnityEngine.UI;
+
+    [RequireComponent(typeof(Button))]
+    public class UIButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        bool m_pressed = false;
+        public bool Pressed{
+            get{
+                return m_pressed;
+            }
+        }
+        public UnityEvent onPressed = new UnityEvent();
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            m_pressed = true;
+        }
+     
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            m_pressed = false;
+        }
+     
+        void Update()
+        {
+            if (!m_pressed)
+                return;
+            onPressed.Invoke(); 
+            // DO SOMETHING HERE
+        }
+    }
+
+}
---
+++ UnityRSToolkit/Assets/RSToolKit/Scripts/UI/Controls/UIScrollbar.cs
@@ -0,0 +1,50 @@
+﻿namespace RSToolkit.UI.Controls
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+       // Designwise only supports horizonal 
+    public class UIScrollbar : MonoBehaviour
+    {
+        public UIButton ScrollDecButton;
+        public UIButton ScrollIncButton;
+         
+        float m_value = 0f; // Is compensate for scrollbars with steps
+
+        float ScrollSpeed = 0.05f;
+        public Scrollbar ScrollBarComponent;
+        // Start is called before the first frame update
+        void Start()
+        {
+            m_value = ScrollBarComponent.value;
+           if(ScrollDecButton != null){
+            ScrollDecButton.onPressed.AddListener(ScrollDec);
+           } 
+           if(ScrollIncButton != null){
+            ScrollIncButton.onPressed.AddListener(ScrollInc);
+           } 
+           ScrollBarComponent.onValueChanged.AddListener(onValueChanged);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+        void onValueChanged(float val){
+            if(!ScrollDecButton.Pressed && !ScrollIncButton.Pressed){
+                m_value = val;
+            }
+        }
+        void ScrollDec(){
+            m_value -= ScrollSpeed;
+            ScrollBarComponent.value = m_value;
+        }
+        void ScrollInc(){
+            m_value += ScrollSpeed;
+            ScrollBarComponent.value = m_value;
+        }
+    }
+}
