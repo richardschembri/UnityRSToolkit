3dfa208cdc7d9acb81f10b82894e7e38829ce3f8
Richard <richard.schembri@tongullman.co.jp>
2019-08-09 18:21:39 +0900

    Foreground Manager

---
+++ UnityRSToolkit/Assets/RSToolKit/Scripts/UI/Paging/UIPageForegroundManager.cs
@@ -0,0 +1,61 @@
+﻿namespace RSToolkit.UI.Paging
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class UIPageForegroundManager : MonoBehaviour
+    {
+        private UIPageHeader m_uiPageHeader;
+        private UIPageHeader m_UIPageHeader{
+            get{
+                if(m_uiPageHeader == null){
+                    m_uiPageHeader = this.GetComponentInChildren<UIPageHeader>();
+                }
+                return m_uiPageHeader;
+            }
+        }
+
+        IEnumerator Init()
+        {
+            yield return new WaitUntil(() => UIPageManager.Instance.InitComplete);
+            for (int i = 0; i < UIPageManager.Instance.Pages.Length; i++)
+            {
+                var page = UIPageManager.Instance.Pages[i];
+                if (page.DisplayHeader)
+                {
+                    page.OnNavigatedTo.AddListener(onNavigatedTo);
+                }
+                
+            }
+
+            SetHeader(UIPageManager.Instance.CurrentPage);
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+           StartCoroutine(Init()); 
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+        public void SetHeader(UIPage page)
+        {
+            if(m_UIPageHeader != null){
+                if(page.DisplayHeader){
+                    m_UIPageHeader.SetHeaderText(page); 
+                }
+                m_UIPageHeader.gameObject.SetActive(page.DisplayHeader);
+            }
+        }
+        #region Page Events
+        void onNavigatedTo(UIPage page, bool keepCache)
+        {
+            SetHeader(page);
+        }
+        #endregion Page Events
+    }
+}
--- UnityRSToolkit/Assets/RSToolKit/Scripts/UI/Paging/UIPageHeader.cs
+++ UnityRSToolkit/Assets/RSToolKit/Scripts/UI/Paging/UIPageHeader.cs
@@ -7,10 +7,26 @@
 
     public class UIPageHeader : MonoBehaviour
     {
+        private Text m_headerText;
+        private Text m_HeaderText{
+            get{
+                if(m_headerText == null){
+                    m_headerText = this.gameObject.GetComponentInChildren<Text>();
+                }
+                return m_headerText;
+            }
+        }
+
+        public void SetHeaderText(UIPage page)
+        {
+            if(m_HeaderText != null){
+               m_HeaderText.text = page.GetHeader();
+            }
+        }
+
         // Start is called before the first frame update
         void Start()
         {
-            StartCoroutine(Init());
         }
 
         // Update is called once per frame
@@ -19,32 +35,5 @@
 
         }
 
-        IEnumerator Init()
-        {
-            yield return new WaitUntil(() => UIPageManager.Instance.InitComplete);
-            for (int i = 0; i < UIPageManager.Instance.Pages.Length; i++)
-            {
-                var page = UIPageManager.Instance.Pages[i];
-                if (page.DisplayHeader)
-                {
-                    page.OnNavigatedTo.AddListener(onNavigatedTo);
-                }
-                
-            }
-
-            SetHeader(UIPageManager.Instance.CurrentPage);
-        }
-
-        public void SetHeader(UIPage page)
-        {
-            this.gameObject.GetComponentInChildren<Text>().text = page.GetHeader();
-        }
-
-        #region Page Events
-        void onNavigatedTo(UIPage page, bool keepCache)
-        {
-            SetHeader(page);
-        }
-        #endregion Page Events
     }
 }
