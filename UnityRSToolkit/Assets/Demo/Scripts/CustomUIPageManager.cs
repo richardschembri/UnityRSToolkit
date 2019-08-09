0eb52064cb5b554a236efd4babfa43025c6a01fb
Richard <richard.schembri@tongullman.co.jp>
2019-08-09 14:18:38 +0900

    UIPageManager ovveridable Unity functions

---
+++ UnityRSToolkit/Assets/Demo/Scripts/CustomUIPageManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RSToolkit.UI.Paging;
+
+public class CustomUIPageManager : UIPageManager
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+       base.Start(); 
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+       base.Update(); 
+    }
+}
--- UnityRSToolkit/Assets/RSToolKit/Scripts/UI/Paging/UIPageManager.cs
+++ UnityRSToolkit/Assets/RSToolKit/Scripts/UI/Paging/UIPageManager.cs
@@ -62,18 +62,18 @@
         #endregion
         #region Unity Events
 
-        void Awake(){
+        protected virtual void Awake(){
             m_instance = this;
         }
         // Start is called before the first frame update
-        void Start()
+        protected virtual void Start()
         {
             InitPages();
             InitComplete = true;
         }
 
         // Update is called once per frame
-        void Update()
+        protected virtual void Update()
         {
            if (Input.GetKeyUp(ExitKey)){
                Application.Quit();
