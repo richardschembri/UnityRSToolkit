a022ea7a607842f57c8648460e0d4d0fa6ed0110
Richard <richard.schembri@tongullman.co.jp>
2019-08-20 11:49:59 +0900

    Add Fullscreen Manager

---
+++ UnityRSToolkit/Assets/RSToolKit/Scripts/General/Controls/FullscreenManager.cs
@@ -0,0 +1,34 @@
+﻿namespace RSToolkit.Controls
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    [DisallowMultipleComponent]
+    public class FullscreenManager : MonoBehaviour
+    {
+
+        public KeyCode ToggleKey = KeyCode.F;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+           if (Input.GetKeyUp(ToggleKey)){
+               ToggleFullscreen();
+           }
+        }
+        public void ToggleFullscreen(){
+            ToggleFullscreen(!Screen.fullScreen);
+        }
+        public void ToggleFullscreen(bool on){
+            Debug.LogFormat("Toggle Fullscreen {0}", on ? "ON" : "OFF");
+            Screen.fullScreen = on;
+        }
+    }
+}
