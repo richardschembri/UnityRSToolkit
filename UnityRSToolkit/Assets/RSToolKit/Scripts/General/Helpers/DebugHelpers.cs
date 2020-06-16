a0b8e8aa3b2ea4b55f941832e5e22730275948a2
Rick <r.s@g.com>
2020-06-16 19:38:42 +0900

    Add and modify DebugHelpers

---
+++ UnityRSToolkit/Assets/RSToolKit/Scripts/General/Helpers/DebugHelpers.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RSToolkit.Helpers
+{
+    public static class DebugHelpers
+    {
+        public static string GetLogText(string tag, object message, bool includeTimestamp = false)
+        {
+            return $"[{tag}]: {message}";
+        }
+
+        public static void LogInDebugMode(bool debugMode, string tag, object message, bool includeTimestamp = false)
+        {
+            if (!debugMode)
+            {
+                return;
+            }
+
+            Log(tag, message, includeTimestamp);
+        }
+
+        public static void LogErrorInDebugMode(bool debugMode, string tag, object message, bool includeTimestamp = false)
+        {
+            if (!debugMode)
+            {
+                return;
+            }
+
+            LogError(tag, message, includeTimestamp);
+        }
+
+        public static void Log(string tag, object message, bool includeTimestamp = false)
+        {
+            Debug.Log(GetLogText(tag, message, includeTimestamp));
+        }
+
+        public static void LogError(string tag, object message, bool includeTimestamp = false)
+        {
+            Debug.LogError(GetLogText(tag, message, includeTimestamp));
+        }
+    }
+}
