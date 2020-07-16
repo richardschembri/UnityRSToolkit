470f3df0afccd57bf9a923da5cd56169f01ae5d2
Rick <r.s@g.com>
2020-07-16 19:40:27 +0900

    Add AnimationTrigger

--- UnityRSToolkit/Assets/RSToolKit/Scripts/Animation/CharacterAnimParams.cs
+++ UnityRSToolkit/Assets/RSToolKit/Scripts/Animation/CharacterAnimParams.cs
@@ -4,8 +4,35 @@ using UnityEngine;
 
 namespace RSToolkit.Animation
 {
-    public class CharacterAnimParams
+    public static class CharacterAnimParams
     {
+        public class TriggerAnim
+        {
+            public string TriggerName { get; private set; }
+            public int TriggerHash { get; private set; }
+
+            public string GetAnimName(string prefix = "")
+            {
+                return $"{prefix}{TriggerName}";
+            }
+
+            public void SetTrigger(Animator animatorComponent, string prefix = "")
+            {
+                animatorComponent.SetTrigger(TriggerHash);
+            }
+
+            public bool IsPlayingAnimation(Animator animatorComponent, string prefix = "")
+            {
+                return animatorComponent.GetCurrentAnimatorStateInfo(0).IsName(GetAnimName());
+            }
+
+            public TriggerAnim(string triggerName)
+            {
+                TriggerName = triggerName;
+                TriggerHash = Animator.StringToHash(TriggerName);
+            }
+        }
+
         public static readonly int Speed = Animator.StringToHash("Speed");
         public static readonly int IsGrounded = Animator.StringToHash("IsGrounded");
         public static readonly int HorizontalSpeed = Animator.StringToHash("HorizontalSpeed");
@@ -21,11 +48,11 @@ namespace RSToolkit.Animation
 
         public static void TrySetSpeed(Animator target, float value)
         {
-            if(target != null)
+            if (target != null)
             {
                 target.SetFloat(Speed, value);
             }
-            
+
         }
 
         public static void TrySetIsGrounded(Animator target, bool value)
