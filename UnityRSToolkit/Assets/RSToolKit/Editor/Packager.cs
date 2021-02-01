79ac5f01e42b14d111a5dbaaab49b37e0b4e6264
Richard Schembri <richard@localhost.localdomain>
2021-02-01 21:31:59 +0900

    Add packager

---
+++ UnityRSToolkit/Assets/RSToolKit/Editor/Packager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+namespace RSToolkit{
+    public class Packager
+    {
+        [UnityEditor.MenuItem("Tools/RSToolkit/Packager/ExportSpawner")]
+        public static void ExportSpawner()
+        {
+            Debug.Log("Exporting Spawner");
+            var spawnerPaths = new string[]{
+                "Assets/RSToolKit/Scripts/General/Controls/Spawner",
+                "Assets/RSToolKit/Scripts/General/RSMonoBehaviour.cs",
+                "Assets/RSToolKit/Scripts/General/Helpers/TransformHelpers.cs",
+                "Assets/RSToolKit/Scripts/General/Helpers/RandomHelpers.cs",
+                "Assets/RSToolKit/Scripts/General/Helpers/DebugHelpers.cs"
+            };
+            AssetDatabase.ExportPackage(
+                    spawnerPaths,
+                    "rstoolkit-spawner.unitypackage",
+                    ExportPackageOptions.Recurse);
+            Debug.Log("Exported Spawner");
+        }
+    }
+}
