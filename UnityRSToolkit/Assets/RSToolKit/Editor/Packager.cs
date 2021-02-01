391ac5e4a59a6d69c3632531e35f296fc0b8a7d0
Richard Schembri <richard@localhost.localdomain>
2021-02-01 23:38:06 +0900

    Refactor

--- UnityRSToolkit/Assets/RSToolKit/Editor/Packager.cs
+++ UnityRSToolkit/Assets/RSToolKit/Editor/Packager.cs
@@ -4,7 +4,7 @@ using System;
 namespace RSToolkit{
     public class Packager
     {
-        [UnityEditor.MenuItem("Tools/RSToolkit/Packager/ExportSpawner")]
+        [UnityEditor.MenuItem("Tools/RSToolkit/Export Package/Spawner")]
         public static void ExportSpawner()
         {
             Debug.Log("Exporting Spawner");
@@ -20,6 +20,37 @@ namespace RSToolkit{
                     "rstoolkit-spawner.unitypackage",
                     ExportPackageOptions.Recurse);
             Debug.Log("Exported Spawner");
+        }
+    
+        [UnityEditor.MenuItem("Tools/RSToolkit/Export Package/UI/Paging")]
+        public static void ExportUIPaging(){
+
+            Debug.Log("Exporting UI Paging");
+            var spawnerPaths = new string[]{
+                "Assets/RSToolKit/Scripts/UI/Paging",
+                "Assets/RSToolKit/Prefabs/UI/Paging",
+                "Assets/RSToolKit/Scripts/General/Collections/SizedStack.cs"
+            };
+            AssetDatabase.ExportPackage(
+                    spawnerPaths,
+                    "rstoolkit-uipaging.unitypackage",
+                    ExportPackageOptions.Recurse);
+            Debug.Log("Exported UI Paging");
+        }
+    
+        [UnityEditor.MenuItem("Tools/RSToolkit/Export Package/UI/Controls/Popup")]
+        public static void ExportUIControlsPopup(){
+
+            Debug.Log("Exporting UI Controls Popup");
+            var spawnerPaths = new string[]{
+                "Assets/RSToolKit/Scripts/UI/Controls/UIPopup.cs",
+                "Assets/RSToolKit/Prefabs/UI/Controls/UI Popup.prefab"
+            };
+            AssetDatabase.ExportPackage(
+                    spawnerPaths,
+                    "rstoolkit-uipopup.unitypackage",
+                    ExportPackageOptions.Recurse);
+            Debug.Log("Exported UI Controls Popup");
         }
     }
 }
