4513af957da7ef54492d5d5bce8b04cea09db104
Richard Schembri <richard@localhost.localdomain>
2021-02-06 23:06:52 +0900

    Add parallax scrolling and assets

---
+++ UnityRSToolkit/Assets/Demo/Space2D/ParallaxBackground/Scripts/DemoCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoCamera : MonoBehaviour
+{
+    [SerializeField] private float _speed = .1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(Input.GetKey(KeyCode.D)){
+            this.transform.position =  this.transform.position + new Vector3(_speed, 0f, 0f);
+        }else if(Input.GetKey(KeyCode.A)){
+            this.transform.position =  this.transform.position + new Vector3(-_speed, 0f, 0f);
+        }
+        
+    }
+}
---
+++ UnityRSToolkit/Assets/RSToolKit/Scripts/Space2D/ParallaxBackground.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RSToolkit.Space2D
+{
+    public class ParallaxBackground : MonoBehaviour
+    {
+        [SerializeField] private float _parallaxEffectMultiplier; 
+
+        private Transform _cameraTransform;
+        private Vector3 _lastCameraPosition;        
+        private Vector3 _deltaMovement;
+
+        private float _textureUnitSizeX;
+        private float _offsetPositionX;
+
+        private void Start(){
+            _cameraTransform = Camera.main.transform;
+            _lastCameraPosition = _cameraTransform.position;
+            Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+            _textureUnitSizeX = sprite.texture.width / sprite.pixelsPerUnit;
+        }
+        private void LateUpdate(){
+            _deltaMovement = _cameraTransform.position - _lastCameraPosition;
+            
+            transform.position += new Vector3(_deltaMovement.x * _parallaxEffectMultiplier, _deltaMovement.y * _parallaxEffectMultiplier, 0f);
+            _lastCameraPosition = _cameraTransform.position;
+            if (Mathf.Abs(_cameraTransform.position.x - transform.position.x) >= _textureUnitSizeX){
+                _offsetPositionX = (_cameraTransform.position.x - transform.position.x) % _textureUnitSizeX;
+                transform.position = new Vector3(_cameraTransform.position.x, transform.position.y);
+            }
+        }
+    }
+}
