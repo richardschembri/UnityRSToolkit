9abdc48044cec43e75ab87f2131593678bdaa207
Richard Schembri <richard@localhost.localdomain>
2021-02-07 21:16:39 +0900

    Camera follow and add mage sprites

---
+++ UnityRSToolkit/Assets/RSToolKit/Scripts/Space2D/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RSToolkit.Space2D
+{
+
+    public class CameraFollow : MonoBehaviour
+    {
+        public Transform target;
+        public Vector3 offset;
+        [Range(1,10)]
+        public float smoothFactor;
+        private Vector3 _targetPosition;
+        private Vector3 _smoothPosition;
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+        }
+
+        void Follow(){
+           _targetPosition = target.position + offset;
+           _smoothPosition = Vector3.Lerp(transform.position, _targetPosition, smoothFactor * Time.fixedDeltaTime);
+           target.position = _smoothPosition; 
+        }
+    }
+}
