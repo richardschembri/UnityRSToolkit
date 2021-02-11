98115fad448a7cc16b5fe8793ec017ad7b0fbc3b
Richard Schembri <richard@localhost.localdomain>
2021-02-11 23:49:50 +0900

    Update Parallax Scrolling demo to have a character

--- UnityRSToolkit/Assets/RSToolKit/Scripts/Space2D/CameraFollow2D.cs
+++ UnityRSToolkit/Assets/RSToolKit/Scripts/Space2D/CameraFollow2D.cs
@@ -1,28 +1,28 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace RSToolkit.Space2D
 {
-
-    public class CameraFollow : MonoBehaviour
+    public class CameraFollow2D : MonoBehaviour
     {
         public Transform target;
         public Vector3 offset;
         [Range(1,10)]
-        public float smoothFactor;
+        public float smoothFactor = 1f;
         private Vector3 _targetPosition;
         private Vector3 _smoothPosition;
 
         // Update is called once per frame
         void FixedUpdate()
         {
+            Follow();
         }
 
         void Follow(){
            _targetPosition = target.position + offset;
            _smoothPosition = Vector3.Lerp(transform.position, _targetPosition, smoothFactor * Time.fixedDeltaTime);
-           target.position = _smoothPosition; 
+           transform.position = new Vector3(_smoothPosition.x, transform.position.y, transform.position.z);//_smoothPosition; 
         }
     }
 }
