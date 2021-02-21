3ff9d696ae06f034e02886c5cc3508e5bc0647db
Richard Schembri <richard@localhost.localdomain>
2021-02-21 15:07:41 +0900

    Fix weird merge errors

--- UnityRSToolkit/Assets/RSToolKit/Scripts/Space2D/CameraFollow2D.cs
+++ UnityRSToolkit/Assets/RSToolKit/Scripts/Space2D/CameraFollow2D.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -53,33 +52,3 @@ namespace RSToolkit.Space2D
         }
     }
 }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace RSToolkit.Space2D
-{
-    public class CameraFollow2D : MonoBehaviour
-    {
-        public Transform target;
-        public Vector3 offset;
-        [Range(1,10)]
-        public float smoothFactor = 1f;
-        private Vector3 _targetPosition;
-        private Vector3 _smoothPosition;
-
-        // Update is called once per frame
-        void FixedUpdate()
-        {
-            Follow();
-        }
-
-        void Follow(){
-           _targetPosition = target.position + offset;
-           _smoothPosition = Vector3.Lerp(transform.position, _targetPosition, smoothFactor * Time.fixedDeltaTime);
-           transform.position = new Vector3(_smoothPosition.x, transform.position.y, transform.position.z);//_smoothPosition; 
-        }
-    }
-}
->>>>>>> dfa7bc39c549895533d19d6e45d5922c55741787
